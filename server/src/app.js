c3f5b8e791bbdf47616555a2333dc94936603d2c
gulsane <craftygulsane@gmail.com>
2023-08-14 00:40:54 +0530

    Added: cors in app with methods get, post, patch, delete and put

--- server/src/app.js
+++ server/src/app.js
@@ -1,5 +1,21 @@
+const morgan = require("morgan");
+const rateLimit = require("express-rate-limit");
+const helmet = require("helmet");
+const mongosanitize = require("express-mongo-sanitize");
+const xss = require("xss-clean");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+const session = require("cookie-session");
 const express = require("express");
-
 const app = express();
+
+app.use(
+	cors({
+		origin: "*",
+		methods: ["GET", "PATCH", "POST", "DELETE", "PUT"],
+		credentials: true,
+	})
+);
 
 module.exports = app;
