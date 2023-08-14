1c96db43d6327e21d072b93ab1dcd9cb81c15f68
gulsane <craftygulsane@gmail.com>
2023-08-14 10:38:19 +0530

    Added: mongoose middleware pre on save on userSchema, which update passwordChangeAt if password changed

--- server/models/user.js
+++ server/models/user.js
@@ -94,6 +94,14 @@ userSchema.pre("save", async function (next) {
 	next();
 });
 
+userSchema.pre("save", function (next) {
+	if (!this.isModified("password") || this.isNew || !this.password)
+		return next();
+
+	this.passwordChangedAt = Date.now() - 1000;
+	next();
+});
+
 const User = new mongoose.model("User", userSchema);
 
 module.exports = User;
