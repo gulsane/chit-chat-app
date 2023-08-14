f7150c3bfce67a5dcfcbd86b34186c814da43f9f
gulsane <craftygulsane@gmail.com>
2023-08-14 13:23:09 +0530

    Added: route for send-otp in auth router

--- server/src/routes/auth.js
+++ server/src/routes/auth.js
@@ -2,5 +2,6 @@ const router = require("express").Router();
 const authController = require("../controllers/authController");
 
 router.post("/register", authController.register, authController.sendOTP);
+router.post("/send-otp", authController.sendOTP);
 
 module.exports = router;
