93b70a333d1737738badd73d39240e58e3deb180
gulsane <craftygulsane@gmail.com>
2023-08-14 12:55:29 +0530

    Modified Code: extracted try-catch functionality from register and sendOTP auth controllers

--- server/src/controllers/authController.js
+++ server/src/controllers/authController.js
@@ -1,68 +1,66 @@
 const User = require("../models/user");
 const otpGenerator = require("otp-generator");
 
-const register = async (req, res, next) => {
-	try {
-		const { firstName, lastName, email, password } = req.body;
-
-		const existing_user = User.findOne({ email: email });
-
-		if (existing_user && existing_user.verified) {
-			res.status(400).json({
-				status: "error",
-				message: "Email already in use, Please login",
-			});
-		} else if (existing_user) {
-			await User.findOneAndUpdate(
-				{ email: email },
-				{ firstName, lastName, email, password },
-				{ new: true, validateModifiedOnly: true }
-			);
-			req.user_id = existing_user._id;
-			next();
-		} else {
-			const new_user = await User.create({
-				firstName,
-				lastName,
-				email,
-				password,
-			});
-			req.user_id = new_user._id;
-			next();
-		}
-	} catch (error) {
-		next(error);
-	}
+const catchAsync = (func) => {
+	return (req, res, next) => {
+		func(req, res, next).catch((err) => next(err));
+	};
 };
 
-const sendOTP = async (req, res, next) => {
-	try {
-		const { user_id } = req;
+const register = catchAsync(async (req, res, next) => {
+	const { firstName, lastName, email, password } = req.body;
 
-		const new_otp = otpGenerator.generate(6, {
-			upperCaseAlphabets: false,
-			specialChars: false,
-			lowerCaseAlphabets: false,
+	const existing_user = User.findOne({ email: email });
+
+	if (existing_user && existing_user.verified) {
+		res.status(400).json({
+			status: "error",
+			message: "Email already in use, Please login",
 		});
-
-		const otp_expiry_time = Date.now() + 10 * 60 * 1000;
-
-		const user = await User.findByIdAndUpdate(user_id, {
-			otp_expiry_time: otp_expiry_time,
+	} else if (existing_user) {
+		await User.findOneAndUpdate(
+			{ email: email },
+			{ firstName, lastName, email, password },
+			{ new: true, validateModifiedOnly: true }
+		);
+		req.user_id = existing_user._id;
+		next();
+	} else {
+		const new_user = await User.create({
+			firstName,
+			lastName,
+			email,
+			password,
 		});
-
-		user.otp = new_otp.toString();
-		await user.save({ new: true, validateModifiedOnly: true });
-
-		//TODO -> Add mailer service here
-
-		res.status(200).json({
-			status: "success",
-			message: "OTP Sent Successfully!",
-		});
-	} catch (error) {
-		next(error);
+		req.user_id = new_user._id;
+		next();
 	}
-};
+});
+
+const sendOTP = catchAsync(async (req, res, next) => {
+	const { user_id } = req;
+
+	const new_otp = otpGenerator.generate(6, {
+		upperCaseAlphabets: false,
+		specialChars: false,
+		lowerCaseAlphabets: false,
+	});
+
+	const otp_expiry_time = Date.now() + 10 * 60 * 1000;
+
+	const user = await User.findByIdAndUpdate(user_id, {
+		otp_expiry_time: otp_expiry_time,
+	});
+
+	user.otp = new_otp.toString();
+	await user.save({ new: true, validateModifiedOnly: true });
+
+	//TODO -> Add mailer service here
+
+	res.status(200).json({
+		status: "success",
+		message: "OTP Sent Successfully!",
+	});
+});
 
 module.exports = { register, sendOTP };
