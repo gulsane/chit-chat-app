eda0957c88406c9ade7981940a60223f7d21d8e3
gulsane <craftygulsane@gmail.com>
2023-08-14 13:21:45 +0530

    Added: route for register in auth router

--- server/src/routes/auth.js
+++ server/src/routes/auth.js
@@ -1,3 +1,6 @@
 const router = require("express").Router();
+const authController = require("../controllers/authController");
+
+router.post("/register", authController.register, authController.sendOTP);
 
 module.exports = router;
