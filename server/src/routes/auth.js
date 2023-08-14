f9511481b8e39b5d8c4123ee677b11fd3f586169
gulsane <craftygulsane@gmail.com>
2023-08-14 17:08:51 +0530

    Added: login router to auth router

--- server/src/routes/auth.js
+++ server/src/routes/auth.js
@@ -4,5 +4,6 @@ const authController = require("../controllers/authController");
 router.post("/register", authController.register, authController.sendOTP);
 router.post("/send-otp", authController.sendOTP);
 router.post("/verify-otp", authController.verifyOTP);
+router.post("/login", authController.login);
 
 module.exports = router;
