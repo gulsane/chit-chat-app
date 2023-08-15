7a3eba00d82db251ee040ad3c12a7cb067377040
gulsane <craftygulsane@gmail.com>
2023-08-15 17:21:53 +0530

    Added: mongodb connection to in server.js file

--- server/server.js
+++ server/server.js
@@ -1,10 +1,21 @@
+const mongoose = require("mongoose");
+mongoose.set({ strictQuery: true });
 const dotenv = require("dotenv");
 dotenv.config({ path: "./config.env" });
 
 const app = require("./src/app.js");
 
 const PORT = process.env.PORT || 8000;
-
 app.listen(PORT, () => {
 	console.log(`Server is running on PORT : ${PORT}`);
 });
+
+const DATABASE = process.env.DATABASE;
+mongoose
+	.connect(DATABASE, {
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+	})
+	.then(() => {
+		console.log("DB connection succefully");
+	});
