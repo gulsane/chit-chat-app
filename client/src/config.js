506f3261eb2159cef3cf00ea56e1a50f61bfcaad
gulsane <craftygulsane@gmail.com>
2023-08-16 17:11:39 +0530

    Added : config.js file, which holds data about theme settings

---
+++ client/src/config.js
@@ -0,0 +1,8 @@
+export const defaultSettings = {
+	themeMode: "light",
+	themeDirection: "ltr",
+	themeContrast: "default",
+	themeLayout: "horizontal",
+	themeColorPresets: "default",
+	themeStretch: false,
+};
