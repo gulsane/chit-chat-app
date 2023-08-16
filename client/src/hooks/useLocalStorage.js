db4317ea6b71f2fed057c77ad2489b9d33cba635
gulsane <craftygulsane@gmail.com>
2023-08-16 16:54:34 +0530

    Added Hook: useLocalStorage, which is used to set items to the localstorage, works as useState

---
+++ client/src/hooks/useLocalStorage.js
@@ -0,0 +1,35 @@
+import { useState, useEffect } from "react";
+
+const useLocalStorage = (key, defaultValue = {}) => {
+	const [value, setValue] = useState(() => {
+		const storedValue = localStorage.getItem(key);
+
+		return storedValue === null ? defaultValue : JSON.parse(storedValue);
+	});
+
+	useEffect(() => {
+		const listener = (e) => {
+			if (e.storageArea === localStorage && e.key === key) {
+				setValue(JSON.parse(e.newValue));
+			}
+		};
+
+		window.addEventListener("storage", listener);
+
+		return () => {
+			window.removeEventListener("storage", listener);
+		};
+	}, [key, defaultValue]);
+
+	const setValueInLocalStorage = (newValue) => {
+		setValue((currVal) => {
+			const result = typeof newValue === "function" ? newValue(currVal) : newValue;
+			localStorage.setItem(key, JSON.stringify(result));
+			return result;
+		});
+	};
+
+	return [value, setValueInLocalStorage];
+};
+
+export default useLocalStorage;
