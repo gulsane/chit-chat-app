ceac00332af3707767783e390ad994f3539d230f
gulsane <craftygulsane@gmail.com>
2023-08-18 13:11:34 +0530

    Modified: moved NAVBAR inside config.js file and changes drawer according to it

--- client/src/components/settings/drawer/index.js
+++ client/src/components/settings/drawer/index.js
@@ -4,16 +4,7 @@ import { Backdrop, IconButton, Stack, Typography } from "@mui/material";
 import { AnimatePresence, m } from "framer-motion";
 import cssStyles from "../../../utils/cssStyles";
 import Iconify from "../../Iconify";
-
-const NAVBAR = {
-	BASE_WIDTH: 260,
-	DASHBOARD_WIDTH: 280,
-	DASHBOARD_COLLAPSE_WIDTH: 88,
-	//
-	DASHBOARD_ITEM_ROOT_HEIGHT: 48,
-	DASHBOARD_ITEM_SUB_HEIGHT: 40,
-	DASHBOARD_ITEM_HORIZONTAL_HEIGHT: 32,
-};
+import { NAVBAR } from "../../../config";
 
 const RootStyle = styled(m.div)(({ theme }) => ({
 	...cssStyles(theme).bgBlur({
--- client/src/config.js
+++ client/src/config.js
@@ -42,4 +42,13 @@ export const allLangs = [
 	},
 ];
 
+export const NAVBAR = {
+	BASE_WIDTH: 260,
+	DASHBOARD_WIDTH: 280,
+	DASHBOARD_COLLAPSE_WIDTH: 88,
+	DASHBOARD_ITEM_ROOT_HEIGHT: 48,
+	DASHBOARD_ITEM_SUB_HEIGHT: 40,
+	DASHBOARD_ITEM_HORIZONTAL_HEIGHT: 32,
+};
+
 export const defaultLang = allLangs[0];
