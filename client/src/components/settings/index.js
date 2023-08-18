079a1b0a5dd17f4e9c320e4ab70a3f8da4d6bb18
gulsane <craftygulsane@gmail.com>
2023-08-18 10:12:20 +0530

    Added: ThemeContrast, which provides contrast to app

--- client/src/components/settings/index.js
+++ client/src/components/settings/index.js
@@ -1,10 +1,15 @@
 import PropTypes from "prop-types";
 import ThemeColorPresets from "./ThemeColorPresets";
+import ThemeContrast from "./ThemeContrast";
 
 ThemeSettings.propTypes = {
 	children: PropTypes.node.isRequired,
 };
 
 export default function ThemeSettings({ children }) {
-	return <ThemeColorPresets>{children}</ThemeColorPresets>;
+	return (
+		<ThemeColorPresets>
+			<ThemeContrast>{children}</ThemeContrast>
+		</ThemeColorPresets>
+	);
 }
