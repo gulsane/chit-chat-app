ae6baf988a1e59e38e84ee9106947c5e642a7f45
gulsane <craftygulsane@gmail.com>
2023-08-18 10:20:29 +0530

    Added: language configuration in config.js file

--- client/src/config.js
+++ client/src/config.js
@@ -1,3 +1,5 @@
+import { enUS, frFR, zhCN, viVN, arSD } from "@mui/material/locale";
+
 export const defaultSettings = {
 	themeMode: "light",
 	themeDirection: "ltr",
@@ -6,3 +8,38 @@ export const defaultSettings = {
 	themeColorPresets: "default",
 	themeStretch: false,
 };
+
+export const allLangs = [
+	{
+		label: "English",
+		value: "en",
+		systemValue: enUS,
+		icon: "/assets/icons/flags/ic_flag_en.svg",
+	},
+	{
+		label: "French",
+		value: "fr",
+		systemValue: frFR,
+		icon: "/assets/icons/flags/ic_flag_fr.svg",
+	},
+	{
+		label: "Vietnamese",
+		value: "vn",
+		systemValue: viVN,
+		icon: "/assets/icons/flags/ic_flag_vn.svg",
+	},
+	{
+		label: "Chinese",
+		value: "cn",
+		systemValue: zhCN,
+		icon: "/assets/icons/flags/ic_flag_cn.svg",
+	},
+	{
+		label: "Arabic (Sudan)",
+		value: "ar",
+		systemValue: arSD,
+		icon: "/assets/icons/flags/ic_flag_sa.svg",
+	},
+];
+
+export const defaultLang = allLangs[0];
