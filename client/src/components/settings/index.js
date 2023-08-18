d9a82be84c6fed1d3784fa4e65a5a91c004b2368
gulsane <craftygulsane@gmail.com>
2023-08-18 10:07:08 +0530

    Added: ThemeColorPresets, which provides color presets to app

--- client/src/components/settings/index.js
+++ client/src/components/settings/index.js
@@ -1,9 +1,10 @@
 import PropTypes from "prop-types";
+import ThemeColorPresets from "./ThemeColorPresets";
 
 ThemeSettings.propTypes = {
 	children: PropTypes.node.isRequired,
 };
 
 export default function ThemeSettings({ children }) {
-	return <>{children}</>;
+	return <ThemeColorPresets>{children}</ThemeColorPresets>;
 }
