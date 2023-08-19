cd153bfb4212ce12e74195b06e5057b7728dd117
gulsane <craftygulsane@gmail.com>
2023-08-19 23:19:53 +0530

    Added: logo icon in auth layout

--- client/src/layouts/auth/index.js
+++ client/src/layouts/auth/index.js
@@ -1,13 +1,15 @@
 import { Container, Stack } from "@mui/material";
 import { Outlet } from "react-router-dom";
 
+import Logo from "../../assets/Images/logo.ico";
+
 const AuthLayout = () => {
 	return (
 		<>
 			<Container sx={{ mt: 5 }} maxWidth="sm">
 				<Stack spacing={5}>
 					<Stack sx={{ width: "100%" }} direction="column" alignItems={"center"}>
-						Auth logo icon
+						<img style={{ height: 120, width: 120 }} src={Logo} alt="Logo" />
 					</Stack>
 					<Outlet />
 				</Stack>
