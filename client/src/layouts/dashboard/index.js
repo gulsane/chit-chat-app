04c254f67806a6364120b16644d922fdef3b6d3e
gulsane <craftygulsane@gmail.com>
2023-08-29 04:48:24 +0530

    Modified: DashboardLayout component, navigating to login page if user is not logged in

--- client/src/layouts/dashboard/index.js
+++ client/src/layouts/dashboard/index.js
@@ -1,4 +1,13 @@
+import { Navigate } from "react-router-dom";
+import { useSelector } from "react-redux";
+
 const DashboardLayout = () => {
+	const { isLoggedIn } = useSelector((state) => state.auth);
+
+	if (!isLoggedIn) {
+		return <Navigate to={"/auth/login"} />;
+	}
+
 	return <h1>this is chatting app</h1>;
 };
 
