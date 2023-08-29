fb0833a7f1d9f006849f139e2b7ed8180116c453
gulsane <craftygulsane@gmail.com>
2023-08-29 06:23:06 +0530

    Added: login Schema for email and password and form provider

--- client/src/sections/auth/LoginForm.js
+++ client/src/sections/auth/LoginForm.js
@@ -1,5 +1,46 @@
+import * as Yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
+import FormProvider from "../../components/hook-form";
+
 const LoginForm = () => {
-	return <div> this is login form</div>;
+	const loginSchema = Yup.object().shape({
+		email: Yup.string()
+			.required("Email is required")
+			.email("Email must be a valid email id"),
+		password: Yup.string()
+			.required("Password is required")
+			.min(8, "Password should be minimum of 8 characters")
+			.max(16, "Password should be maximum of 16 characters"),
+	});
+
+	const defaultValues = {
+		email: "",
+		password: "",
+	};
+
+	const methods = yupResolver(loginSchema, defaultValues);
+
+	const { reset, setError, handleSubmit } = methods;
+
+	const onSubmit = async (data) => {
+		try {
+			console.log(data);
+			// submit data to backend
+		} catch (error) {
+			console.error(error);
+			reset();
+			setError("afterSubmit", {
+				...error,
+				message: error.message,
+			});
+		}
+	};
+	return (
+		<FormProvider
+			methods={methods}
+			onSubmit={handleSubmit(onSubmit)}
+		></FormProvider>
+	);
 };
 
 export default LoginForm;
