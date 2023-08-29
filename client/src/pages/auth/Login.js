b2c183b2a0564b378b2770669a714e56d59bab1a
gulsane <craftygulsane@gmail.com>
2023-08-29 05:56:07 +0530

    Added: Login Form and made changes in Login page accordingly

--- client/src/pages/auth/Login.js
+++ client/src/pages/auth/Login.js
@@ -1,18 +1,22 @@
 import { Stack, Typography, Link } from "@mui/material";
 import { Link as RouterLink } from "react-router-dom";
+import LoginForm from "../../sections/auth/LoginForm";
 
 const Login = () => {
 	return (
-		<Stack spacing={2} sx={{ mb: 5, position: "relative" }}>
-			<Typography variant="h4">Login to chit-chat</Typography>
-			<Stack direction="row" spacing={0.5}>
-				<Typography variant="body2">New user?</Typography>
+		<>
+			<Stack spacing={2} sx={{ mb: 5, position: "relative" }}>
+				<Typography variant="h4">Login to chit-chat</Typography>
+				<Stack direction="row" spacing={0.5}>
+					<Typography variant="body2">New user?</Typography>
 
-				<Link to={"/auth/register"} component={RouterLink} variant="subtitle2">
-					Create an account
-				</Link>
+					<Link to={"/auth/register"} component={RouterLink} variant="subtitle2">
+						Create an account
+					</Link>
+				</Stack>
 			</Stack>
-		</Stack>
+			<LoginForm />
+		</>
 	);
 };
 
---
+++ client/src/sections/auth/LoginForm.js
@@ -0,0 +1,5 @@
+const LoginForm = () => {
+	return <div> this is login form</div>;
+};
+
+export default LoginForm;
