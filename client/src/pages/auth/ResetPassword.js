787aca819a4224e32ba379f0b55ae3f0e84d8bbb
gulsane <craftygulsane@gmail.com>
2023-08-30 23:32:56 +0530

    Added heading and Link for returning to login page in reset password page

--- client/src/pages/auth/ResetPassword.js
+++ client/src/pages/auth/ResetPassword.js
@@ -1,5 +1,38 @@
+import { Stack, Typography, Link } from "@mui/material";
+import { Link as RouterLink } from "react-router-dom";
+import { CaretLeft } from "phosphor-react";
+
 const ResetPassword = () => {
-	return <div>this is reset password page and need to be implemented</div>;
+	return (
+		<>
+			<Stack spacing={2} sx={{ mb: 5, position: "relative" }}>
+				<Typography variant="h3" paragraph>
+					Forgot your password?
+				</Typography>
+
+				<Typography sx={{ color: "text.secondary", mb: 5 }}>
+					Please enter the email address associated with your account and We will
+					email you a link to reset your password.
+				</Typography>
+			</Stack>
+			//RESET PASSWORD FORM IMPLEMENTATION HERE NEEDED
+			<Link
+				component={RouterLink}
+				to={"/auth/login"}
+				color="inherit"
+				variant="subtitle2"
+				sx={{
+					mt: 3,
+					mx: "auto",
+					alignItems: "center",
+					display: "inline-flex",
+				}}
+			>
+				<CaretLeft size={24} />
+				Return to sign in
+			</Link>
+		</>
+	);
 };
 
 export default ResetPassword;
