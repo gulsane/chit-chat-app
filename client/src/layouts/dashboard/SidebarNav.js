818a67a295264ad0bb582307427daea3a105abaa
gulsane <craftygulsane@gmail.com>
2023-09-01 17:11:08 +0530

    Added: App logo to sidebar nav

--- client/src/layouts/dashboard/SidebarNav.js
+++ client/src/layouts/dashboard/SidebarNav.js
@@ -1,5 +1,42 @@
+import { useTheme } from "@mui/material/styles";
+import { Box, Stack } from "@mui/material";
+import Logo from "../../assets/Images/logo.ico";
+
 const SidebarNav = () => {
-	return <div> this is sidebar</div>;
+	const theme = useTheme();
+	return (
+		<Box
+			sx={{
+				height: "100vh",
+				width: 100,
+				backgroundColor:
+					theme.palette.mode === "light"
+						? "#F0F4FA"
+						: theme.palette.background.paper,
+				boxShadow: "0px 0px 2px rgba(0, 0, 0, 0.25)",
+			}}
+		>
+			<Stack
+				py={3}
+				alignItems={"center"}
+				justifyContent={"space-between"}
+				sx={{ height: "100%" }}
+			>
+				<Stack alignItems={"center"} spacing={4}>
+					<Box
+						sx={{
+							height: 64,
+							width: 64,
+							borderRadius: 1.5,
+							backgroundColor: theme.palette.primary.main,
+						}}
+					>
+						<img src={Logo} alt="chit-chat" />
+					</Box>
+				</Stack>
+			</Stack>
+		</Box>
+	);
 };
 
 export default SidebarNav;
