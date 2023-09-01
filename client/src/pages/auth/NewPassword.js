4c106a8f177a0987fee6cff6c606055af152b2c2
gulsane <craftygulsane@gmail.com>
2023-09-01 13:34:10 +0530

    Modified: added heading and login page link to new password page

--- client/src/pages/auth/NewPassword.js
+++ client/src/pages/auth/NewPassword.js
@@ -1,5 +1,36 @@
+import { Stack, Typography, Link } from "@mui/material";
+import { Link as RouterLink } from "react-router-dom";
+import { CaretLeft } from "phosphor-react";
+
 const NewPassword = () => {
-	return <div>this is NewPassword page and needs to be implemented</div>;
+	return (
+		<>
+			<Stack spacing={2} sx={{ mb: 5, position: "relative" }}>
+				<Typography variant="h3" paragraph>
+					Reset Password
+				</Typography>
+				<Typography sx={{ color: "text.secondary", mb: 5 }}>
+					Set your new password
+				</Typography>
+			</Stack>
+			new password form needs to be added
+			<Link
+				component={RouterLink}
+				to={"/auth/login"}
+				color="inherit"
+				variant="subtitle2"
+				sx={{
+					mt: 3,
+					mx: "auto",
+					alignItems: "center",
+					display: "inline-flex",
+				}}
+			>
+				<CaretLeft size={24} />
+				Return to sign in
+			</Link>
+		</>
+	);
 };
 
 export default NewPassword;
