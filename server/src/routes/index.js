45c3238b02d422fbaa50523690ec997b272fd095
gulsane <craftygulsane@gmail.com>
2023-09-16 11:31:49 +0530

    Added: user routes to main route

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -1,7 +1,9 @@
 const router = require("express").Router();
 
 const authRoute = require("./auth.js");
+const userRoute = require("./user.js");
 
 router.use("/auth", authRoute);
+router.use("/user", userRoute);
 
 module.exports = router;
